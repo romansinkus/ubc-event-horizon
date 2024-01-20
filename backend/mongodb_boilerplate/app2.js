e387d0cc3db45a94582f4f8d95db8e0201c8d41a
Paul Tiberghien <r1e6f@ugrad.cs.ubc.ca>
2024-01-20 14:30:08 -0800

    added mongodb boilerplate

---
+++ backend/mongodb_boilerplate/app2.js
@@ -0,0 +1,7 @@
+//how to access environment variables set in user settings
+
+require("dotenv").config();
+
+// console.log(process.env.PATH);
+console.log(process.env.nw2024_mongodb_key);
+console.log(process.env.NW2024_MONGODB_KEY);
